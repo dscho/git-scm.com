3ba59f2f441bdcc6c075ce869312c243e71ea430
Johannes Schindelin <johannes.schindelin@gmx.de>
2025-08-09 13:05:54 +0200

    install-hugo: support running on Windows, too
    This requires Git Bash; The tell-tale is the environment variable
    `MSYSTEM`, which is set to `MINGW64` for amd64 CPUs. The script will
    install `hugo.exe` into `~/bin`, which is prepended to the `PATH` in
    Git Bash by default.
    
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>

--- script/install-hugo.sh
+++ script/install-hugo.sh
@@ -5,6 +5,21 @@ die() {
     exit 1
 }
 
+case "$MSYSTEM" in
+MINGW64)
+	suffix=_windows-amd64.zip
+	update() {
+		unzip -d $HOME/bin /tmp/hugo$suffix hugo.exe
+	}
+	;;
+*)
+	suffix=_linux-amd64.deb
+	update() {
+		sudo dpkg -i /tmp/hugo$suffix
+	}
+	;;
+esac
+
 HUGO_VERSION=$(sed -n 's/^ *hugo_version: *//p' <hugo.yml) &&
 test -n "$HUGO_VERSION" ||
 die "hugo_version not found in hugo.yml"
@@ -12,6 +27,6 @@ die "hugo_version not found in hugo.yml"
 echo "Upgrading to Hugo v${HUGO_VERSION}" >&2
 
 download_url=https://github.com/gohugoio/hugo/releases/download &&
-curl -Lo /tmp/hugo.deb $download_url/v$HUGO_VERSION/hugo_extended_${HUGO_VERSION}_linux-amd64.deb &&
-sudo dpkg -i /tmp/hugo.deb ||
+curl -Lo /tmp/hugo$suffix $download_url/v$HUGO_VERSION/hugo_extended_${HUGO_VERSION}${suffix} &&
+update ||
 die "Failed to install Hugo version $HUGO_VERSION"
